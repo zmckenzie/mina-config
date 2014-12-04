55f98afffa5463239061b6fce8965974b4079498
David Hopp <david.hopp@agapered.com>
2014-12-04 15:10:48 +0000

    adding more debugging

--- lib/mina-config/tasks.rb
+++ lib/mina-config/tasks.rb
@@ -1,4 +1,7 @@
-puts '*'*200
+puts File.dirname(__FILE__)
+puts Dir["#{File.dirname(__FILE__)}/tasks/*.rake"]
+
 Dir["#{File.dirname(__FILE__)}/tasks/*.rake"].each do |task|
+  puts task
   load task
 end
