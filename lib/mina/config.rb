d3949cd2721da0449063099289a56b219f3c89a9
Zachary Mckenzie <Zachary.Mckenzie@agapered.com>
2015-01-09 17:24:05 -0600

    cluster modifications for iostream

--- lib/mina/config.rb
+++ lib/mina/config.rb
@@ -4,9 +4,10 @@ require 'yaml'
 require 'mina/rvm'
 require 'active_support/core_ext/hash'
 require 'mina/String'
+require 'open4'
 require 'pry'
 
-mdefault_env = fetch(:default_env, 'staging')
+default_env = fetch(:default_env, 'staging')
 config_file = 'config/deploy.yml'
 set :config, YAML.load(File.open(config_file)).with_indifferent_access if File.exists? config_file
 set :rails_env, ENV['to'] || :staging
@@ -18,23 +19,6 @@ unless config.nil?
   set :environments, envs
 end
 
-
-namespace :deploy do
-  desc 'deploy to a cluster of servers'
-  task :cluster do
-
-    config[cluster_key].each do |command|
-        if environments.include? command
-          exec "mina #{command} deploy"
-        else
-          puts %[Environment #{command} not found. Please define it in your deploy.yml.]
-          exit 1
-        end
-
-    end
-
-  end
-end
 
 unless environments.nil?
   environments.each do |environment|
@@ -68,7 +52,9 @@ def setup_environment environment
   set :shared_paths, config[rails_env]['shared_paths']
   set :deploy_to, "/srv/app/#{app}"
   set :ruby_version, File.read('.ruby-version')
+  set :port, config[rails_env]['port'] || '22'
 
+  invoke :"rvm:use[#{ruby_version}]"
 end
 
 namespace :config do
@@ -116,7 +102,32 @@ namespace :config do
       end
     end
   end
-  
+end
+
+namespace :deploy do
+  desc 'deploy to a cluster of servers'
+  task :cluster do
+    config[cluster_key].each do |env|
+      if environments.include? env
+        puts %[Called Deployment for #{env}.]
+        Open3.popen3("mina #{env} deploy") do |stdin, stdout, stderr, thread|
+          { :out => stdout, :err => stderr }.each do |key, stream|
+            Thread.new do
+              until (raw_line = stream.gets).nil? do
+                puts raw_line
+              end
+            end
+          end
+
+          thread.join
+        end
+      else
+        puts %[Environment #{env} not found. Please define it in your deploy.yml.]
+        exit 1
+      end
+    end
+  end
+
   desc "Rolls back the latest release"
   task :rollback => :environment do
     queue! %[echo "-----> Rolling back to previous release for instance: #{domain}"]
@@ -132,7 +143,6 @@ namespace :config do
     queue! %[ls "#{deploy_to}/releases" -Art | sort | tail -n 1 | xargs -I active rm -rf "#{deploy_to}/releases/active"]
   end
 end
-
 namespace :database do
   
   task :set_version => :environment do
